3e51d0d0c55ad3375ae0fe913a57bd91b869dec5
Peiran Quan <quanpeiran@gmail.com>
2020-10-14 17:22:44 +1300

    DbContext 1:07

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,14 @@
+using BackEndAPIHost.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEndAPIHost.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+            
+        }
+        public DbSet<Command> Commands { get; set; }
+    }
+}
