3da57f5b1042b973b27c9a191162efe73cd24b1a
Peiran Quan <quanpeiran@gmail.com>
2020-10-14 14:12:09 +1300

    define data model, interface repo & mock data

---
+++ Data/MockCallsRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using BackEndAPIHost.Models;
+
+namespace BackEndAPIHost.Data
+{
+    public class MockApiCalls : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="build an API hoster", Line="GET request", Platform = "webapp"},
+                new Command{Id=1, HowTo="build API hosters", Line="POST request", Platform = "laptop"},
+                new Command{Id=2, HowTo="build APIs hoster", Line="PUT request", Platform = "desktop"}
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="build an API hoster", Line="GET request", Platform = "webapp"};
+        }
+    }
+}
