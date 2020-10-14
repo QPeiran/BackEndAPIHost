56bcf39765b5bccbd0068804aa4ef0e0dda760c6
Peiran Quan <quanpeiran@gmail.com>
2020-10-14 14:59:57 +1300

    bad practice -- concrete class instance instead of interface implementation

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using BackEndAPIHost.Models;
+using Microsoft.AspNetCore.Mvc;
+using BackEndAPIHost.Data;
+
+namespace BackEndAPIHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockApiCalls _repository = new MockApiCalls();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandsItems = _repository.GetAppCommands();
+
+            return Ok(commandsItems);
+        }
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandsItem = _repository.GetCommandById(id);
+            return Ok(commandsItem);
+        }
+    }
+}
